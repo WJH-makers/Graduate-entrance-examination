e7975eeb5188bea66c3cfd857743756b83ef0f6c
万佳泓 <136443811+WJH-makers@users.noreply.github.com>
2025-12-02 23:10:34 +0800

    Relax stylelint rules for legacy color syntax and vendor prefixes

--- .stylelintrc.cjs
+++ .stylelintrc.cjs
@@ -5,6 +5,11 @@ module.exports = {
   ],
   rules: {
     "selector-class-pattern": null,
-    "custom-property-empty-line-before": null
+    "custom-property-empty-line-before": null,
+    "rule-empty-line-before": null,
+    "color-function-notation": null,
+    "alpha-value-notation": null,
+    "font-family-name-quotes": null,
+    "property-no-vendor-prefix": null
   }
 };
